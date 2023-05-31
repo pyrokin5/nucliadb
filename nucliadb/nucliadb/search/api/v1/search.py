fa8c41622140e38048cce3a485bb014e44e50944
Ferran Llamas <llamas.arroniz@gmail.com>
2023-05-31 10:55:05 +0200

    Pass min_score on SearchRequest

--- nucliadb/nucliadb/search/api/v1/search.py
+++ nucliadb/nucliadb/search/api/v1/search.py
@@ -274,6 +274,7 @@ async def search(
         filters=item.filters,
         faceted=item.faceted,
         sort=sort_options,
+        min_score=item.min_score,
         page_number=item.page_number,
         page_size=item.page_size,
         range_creation_start=item.range_creation_start,
--- nucliadb/nucliadb/search/search/query.py
+++ nucliadb/nucliadb/search/search/query.py
@@ -55,6 +55,7 @@ async def global_query_to_pb(
     page_number: int,
     page_size: int,
     sort: SortOptions,
+    min_score: float,
     advanced_query: Optional[str] = None,
     range_creation_start: Optional[datetime] = None,
     range_creation_end: Optional[datetime] = None,
@@ -71,6 +72,7 @@ async def global_query_to_pb(
     fields = fields or []
 
     request = SearchRequest()
+    request.min_score = min_score
     request.reload = reload
     request.with_duplicates = with_duplicates
 
